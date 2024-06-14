804c86e6624acec74a027c2a027aef3722c826b0
Ibrahim Sowunmi <ibrahims@stripe.com>
2024-06-14 15:00:05 +0100

    feat: loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      <div className='text-center'>
+        <h1 className='text-3xl font-bold'>Loading...</h1>
+      </div>
+    </div>
+  )
+}
+
+export default loading
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,20 +1,11 @@
-'use client'
-
 import React from 'react'
-import { useSearchParams } from 'next/navigation'
+import SuccessPage from '../Components/Success/SuccessPage'
 
-function Page() {
-  const searchParams = useSearchParams()
-  const sessionID = searchParams.get('session_id');
+const page = () => {
 
   return (
-    <div className='flex items-center justify-center min-h-screen'>
-      <div className='text-center'>
-        <h1 className='text-3xl font-bold'>Success!</h1>
-        <p className='mt-4'>Your order has been placed successfully. Your session ID is {sessionID!}</p>
-      </div>
-    </div>
+    <SuccessPage />
   )
 }
 
-export default Page
+export default page
